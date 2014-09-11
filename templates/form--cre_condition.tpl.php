b1cf62f0c68346be1bc8c57692dc12c9ccb0042c
Brady Owens <info@fastglass.net>
2014-09-11 20:53:13 +0200

    Coding cleanup. Signed-off-by:Brady Owens <info@fastglass.net>

--- templates/form--cre_condition.tpl.php
+++ templates/form--cre_condition.tpl.php
@@ -6,6 +6,5 @@
  */
 $form['parameter']['term_id']['settings']['term_operator'] = $form['parameter']['term_operator']['settings']['term_operator'];
 $form['parameter']['term_id']['settings'] = array_reverse($form['parameter']['term_id']['settings'], TRUE);
-unset($form['parameter'] ['term_operator']);
+unset($form['parameter']['term_operator']);
 echo drupal_render_children($form);
-
