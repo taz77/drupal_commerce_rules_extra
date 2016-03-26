4d4275f500047a9df9b172bc5195dbc1c885d35b
Brady Owens <info@fastglass.net>
2016-03-26 12:52:21 +0100

    Code styling. Blank line.

--- templates/form--cre_condition.tpl.php
+++ templates/form--cre_condition.tpl.php
@@ -4,6 +4,7 @@
  * @file
  * Template file for the condition form.
  */
+
 $form['parameter']['term_id']['settings']['term_operator'] = $form['parameter']['term_operator']['settings']['term_operator'];
 $form['parameter']['term_id']['settings'] = array_reverse($form['parameter']['term_id']['settings'], TRUE);
 unset($form['parameter']['term_operator']);
